e33d151875e84dc61dca6614f0d281d3a5d18564
hammad-tayyab <htandft@gmail.com>
2026-03-12 23:11:04 +0500

    updated  py sqrt(x)

--- Easy/sqrt(x).py
+++ Easy/sqrt(x).py
@@ -8,3 +8,22 @@ class Solution:
                 return i
             if i * i > x:
                 return i - 1
+# binary search
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
+        
+        left = 1  # left number
+        right = x // 2  # middle of the number
+         
+        while left <= right:
+            mid = (left + right) // 2  #assigns the middle of left and right       
+            if mid * mid == x: # checking the exact answer
+                return mid
+            elif mid * mid < x:
+                left = mid + 1      # if its small go right
+            else:
+                right = mid - 1     # if its too big, go left
+        
+        return right
